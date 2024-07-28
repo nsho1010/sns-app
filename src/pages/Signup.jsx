d68b33c5e3611d7a87023eee4209d931059bbc19
Shohei Nagamura <shohei.nagamura@gmail.com>
2024-07-28 13:09:31 +0900

    ユーザー登録機能実装

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+import { authRepository } from "../repositories/auth";
+
 const Signup = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signup = async () => {
+        const user = await authRepository.signup(name, email, password);
+        console.log(user);
+    };
+
     return (
         <div className="min-h-screen bg-gray-100 py-10 px-4 sm:px-6 lg:px-8">
             <div className="flex flex-col items-center">
@@ -17,6 +29,9 @@ const Signup = () => {
                                 </label>
                                 <div className="mt-1">
                                     <input
+                                        onChange={(e) =>
+                                            setName(e.target.value)
+                                        }
                                         id="username"
                                         name="username"
                                         placeholder="ユーザー名"
@@ -35,6 +50,9 @@ const Signup = () => {
                                 </label>
                                 <div className="mt-1">
                                     <input
+                                        onChange={(e) =>
+                                            setEmail(e.target.value)
+                                        }
                                         id="email"
                                         name="email"
                                         placeholder="メールアドレス"
@@ -53,6 +71,9 @@ const Signup = () => {
                                 </label>
                                 <div className="mt-1">
                                     <input
+                                        onChange={(e) =>
+                                            setPassword(e.target.value)
+                                        }
                                         id="password"
                                         name="password"
                                         placeholder="パスワード"
@@ -63,7 +84,15 @@ const Signup = () => {
                                 </div>
                             </div>
                             <div>
-                                <button className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50 disabled:cursor-not-allowed">
+                                <button
+                                    onClick={signup}
+                                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50 disabled:cursor-not-allowed"
+                                    disabled={
+                                        name === "" ||
+                                        email === "" ||
+                                        password === ""
+                                    }
+                                >
                                     登録
                                 </button>
                             </div>
